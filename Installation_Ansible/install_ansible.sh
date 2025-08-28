857c506ac874250ba69e18ba7b79606a4344ae2a
Petrov Evgeny <PetrovE2@ya.ru>
2025-08-28 16:21:02 +0300

    adding color text output functions

--- Installation_Ansible/install_ansible.sh
+++ Installation_Ansible/install_ansible.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+### ЦВЕТА ###
+ESC=$(printf '\033') RESET="${ESC}[0m" MAGENTA="${ESC}[35m" RED="${ESC}[31m" GREEN="${ESC}[32m"
+
+### Функции цветного вывода ###
+magentaprint() { echo; printf "${MAGENTA}%s${RESET}\n" "$1"; }
+errorprint() { echo; printf "${RED}%s${RESET}\n" "$1"; }
+greenprint() { echo; printf "${GREEN}%s${RESET}\n" "$1"; }
+
 # Определение дистрибутива:
 OS=$(awk -F= '/^ID=/{gsub(/"/, "", $2); print $2}' /etc/os-release)
 
@@ -13,22 +21,26 @@ if [ -z "$SUDO_USER" ]; then
     exit 1
 fi
 
-# Установить и настроить Ansible для Ubuntu
+
+# Установить Ansible для Ubuntu
 install_ansible_ubuntu() {
-    # Установить Ansible
+    magentaprint "Установка Ansible на $OS"
     apt install -y ansible
 }
 
-# Установить и настроить Ansible для AlmaLinux
+
+# Установить Ansible для AlmaLinux
 install_ansible_almalinux() {
-    # Установить репозиторий EPEL
+    magentaprint "Установка epel-release"
     dnf install -y epel-release
-    # Установить Ansible
+    magentaprint "Установка Ansible на $OS"
     dnf install -y ansible
 }
 
+
 # Создать конфигурационный файл Ansible
 create_ansible_config() { 
+    magentaprint "Создание конфигурационного файла /etc/ansible/ansible.cfg"
     mkdir -p /etc/ansible
     tee /etc/ansible/ansible.cfg > /dev/null <<EOL
 [defaults]
@@ -41,8 +53,10 @@ interpreter_python = /usr/bin/python3
 EOL
 }
 
+
 # Обновить файл /etc/hosts
 update_hosts_file() {
+    magentaprint "Обновление файла /etc/hosts"
     tee -a /etc/hosts > /dev/null <<EOL
 10.100.10.1 node-vm01
 10.100.10.2 node-vm02
@@ -52,8 +66,10 @@ update_hosts_file() {
 EOL
 }
 
+
 # Создать директорию ansible и файл Inventory
 create_inventory() {
+    magentaprint "Создание Inventory файла /srv/ansible/inventory.ini"
     mkdir -p /srv/ansible
     tee /srv/ansible/inventory.ini > /dev/null <<EOL
 [almalinux]
@@ -72,8 +88,10 @@ node-vm[01:05]
 EOL
 }
 
+
 # Создать файл переменных для группы хостов [myservers]
 create_group_vars() {
+    magentaprint "Создание файла переменных для группы хостов [myservers]"
     mkdir -p /srv/ansible/group_vars
     tee /srv/ansible/group_vars/myservers > /dev/null <<EOL
 # Переменные для группы myservers
@@ -89,16 +107,17 @@ ansible_ssh_privat_key_file : /root/.ssh/authorized_keys
 EOL
 }
 
+
 # Проверить операционную систему и установить Ansible
 if [ "$OS" == "ubuntu" ]; then
     install_ansible_ubuntu
 elif [ "$OS" == "almalinux" ]; then
     install_ansible_almalinux
 else
-    echo "Скрипт не поддерживает установленную операционную систему: $OS"
-    # Выход из скрипта с кодом 1
+    errorprint "Скрипт не поддерживает установленную операционную систему: $OS"
     exit 1
 fi
+
 
 # Настроить Ansible
 create_ansible_config
@@ -106,8 +125,13 @@ update_hosts_file
 create_inventory
 create_group_vars
 
-# Проверить установку Ansible
+# Команда добавляет Python 3 как альтернативу для команды python, устанавливая при этом его приоритет равным 2.
+# После выполнения этой команды, если ввести python в терминале, будет вызываться Python 3
+update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+
+magentaprint "Версия Ansible:"
 ansible --version
 
+
 # Вывести сообщение об успешной установке и настройке
-echo "Ansible успешно установлен и настроен на $OS."
+greenprint "Ansible успешно установлен и настроен на $OS."
